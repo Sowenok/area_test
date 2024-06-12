b555c2ae65434ea55270c5f7c367f2517f2979e0
Бойко Вячеслав <slavikchyharik@gmail.com>
2024-06-12 14:43:09 +0300

    Исправлены ошибки #2

--- lr3/Program.cs
+++ lr3/Program.cs
@@ -6,12 +6,13 @@ namespace lr3
     {
         static void Main(string[] args)
         {
+            double A, B, C, p, S;
             double a, b, Rad;
             int variant;
 
             Console.WriteLine("1. Прямоугольник");
             Console.WriteLine("2. Круг");
-            //Console.WriteLine("3. Треугольник");
+            Console.WriteLine("3. Треугольник");
             //Console.WriteLine("4. Трапеция");
             //Console.WriteLine("5. Сектор");
             
@@ -50,6 +51,23 @@ namespace lr3
                         Console.WriteLine("Площадь круга = " + Math.PI * Math.Pow(Rad, 2));
                         break;
 
+                    case 3:
+                        Console.WriteLine("Введите сторону A: ");
+                        A = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Введите сторону B: ");
+                        B = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Введите сторону C: ");
+                        C = Convert.ToDouble(Console.ReadLine());
+
+                        p = (A + B + C) / 2;
+
+                        S = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+                        Console.WriteLine("Площадь равна: {0}", S);
+                        Console.ReadLine();
+                        break;
 
                     default: Console.WriteLine("Выбор неверен "); break;
 
